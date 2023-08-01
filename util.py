c94657364df83989de129a5b66bf43c73d840e3b
cheveyo1 <138849762+cheveyo1@users.noreply.github.com>
2023-08-01 20:58:38 +0800

    Update util.py

--- util.py
+++ util.py
@@ -1,10 +1,3 @@
-"""
-# -*- encoding: utf-8 -*-
-@Time    :   2021/12/23 14:37:06
-@Author  :   yylime
-@Contact :   844202100@qq.com
-"""
-
 import random
 from selenium.webdriver import ActionChains
 from selenium import webdriver
@@ -19,10 +12,19 @@ import time
 import matplotlib.pylab as plt
 import base64
 import calendar
-from setting import Config
 import argparse
 from imutils import paths
-import cv2
+import os
+from PIL import Image
+from PIL import ImageChops
+import shutil
+import hashlib
+    
+background_path = 'background.png'
+slider_path = 'slider.png'
+offset=-8
+
+
 
 def valid_test_img_show(img, distance):
     plt.figure
@@ -76,208 +78,317 @@ def get_tracks(distance):
     return tracks
 
 
-class Slider:
-    def __init__(self, cfg) -> None:
 
-        options = webdriver.ChromeOptions()
-        #options.add_argument("start-maximized")
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_experimental_option('excludeSwitches', ['enable-automation'])
-        options.add_experimental_option('useAutomationExtension', False)
 
-        self.driver = webdriver.Chrome(
-            options=options
+
+def download_img(driver, selenium_item,path, size):
+
+    url = selenium_item.get_attribute("src")
+
+    if url is not None:
+        response = requests.get(url)
+        with open(path, "wb") as f:
+            f.write(response.content)
+        img = Image.open(path).resize(size)
+        img.save(path)
+        img.close()
+
+    # use js to download picture
+    else:
+        class_name = selenium_item.get_attribute("class")
+        # 下面的js代码根据canvas文档说明而来
+        js_cmd = (
+            'return document.getElementsByClassName("%s")[0].toDataURL("image/png");'
+            % class_name
         )
-        self.driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
-        'source': 'Object.defineProperty(navigator, "webdriver", {get: () => undefined})'
-        })
-        self.wait = WebDriverWait(self.driver, 5)
-        self.driver.get(cfg.TEST_URL)
-        # prepare to do
-        self._prepare(cfg)
-
-
-    def _prepare(self, cfg):
-        # 初始化浏览器
-        self.driver.refresh()
-        # 点击对应标签
-        button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, cfg.HD_SELECOTR)))
-        button.click()
-        self.tc_item = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, cfg.TC_SELECOTR)))
-        self.tc_item.click()
-
-        # 得到背景和滑块的item, 以及滑动按钮
-        time.sleep(3)
-        self.background_item = self.wait.until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, cfg.BG_SELECOTR))
-        )
-        self.slider_item = self.wait.until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, cfg.HK_SELECOTR))
-        )
-        self.slider_btn = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, cfg.HD_BTN)))
-
-        self.offset = cfg.offset
-        
-       
-        self.background_path = cfg.background_path
-        self.slider_path = cfg.slider_path
-
-
-
-    def _download_img(self, selenium_item, path, size):
-        url = selenium_item.get_attribute("src")
-        if url is not None:
-            response = requests.get(url)
-            with open(path, "wb") as f:
-                f.write(response.content)
-            img = Image.open(path).resize(size)
-            img.save(path)
-        # use js to download picture
-        else:
-            class_name = selenium_item.get_attribute("class")
-            # 下面的js代码根据canvas文档说明而来
-            js_cmd = (
-                'return document.getElementsByClassName("%s")[0].toDataURL("image/png");'
-                % class_name
-            )
-            # 执行 JS 代码并拿到图片 base64 数据
-            im_info = self.driver.execute_script(js_cmd)  # 执行js文件得到带图片信息的图片数据
-            im_base64 = im_info.split(",")[1]  # 拿到base64编码的图片信息
-            im_bytes = base64.b64decode(im_base64)  # 转为bytes类型
-            with open(path, "wb") as f:  # 保存图片到本地
-                f.write(im_bytes)
-            img = Image.open(path).resize(size)
-            img.save(path)
+        # 执行 JS 代码并拿到图片 base64 数据
+        im_info = driver.execute_script(js_cmd)  # 执行js文件得到带图片信息的图片数据
+        im_base64 = im_info.split(",")[1]  # 拿到base64编码的图片信息
+        im_bytes = base64.b64decode(im_base64)  # 转为bytes类型
+        with open(path, "wb") as f:  # 保存图片到本地
+            f.write(im_bytes)
+        img = Image.open(path).resize(size)
+        img.save(path)
+        img.close()
     
 
 
-    def _download_images(self,rtime):
-        # mark the size of background picture
-        backgroud_size = (
-            self.background_item.size["width"],
-            self.background_item.size["height"],
-        )
-        slider_size = self.slider_item.size["width"], self.slider_item.size["height"]
-        # download img
-        self._download_img(self.background_item, self.background_path, backgroud_size)
-        self._download_img(self.slider_item, 'imgs/'+rtime+'_'+self.slider_path, slider_size)
+def download_images(driver,background_item,slider_item,rtime):
 
-    def is_image_file(self,filename):
-        return any(filename.endswith(extension) for extension in ['.png', '.jpg', '.jpeg', '.PNG', '.JPG', '.JPEG'])
+    backgroud_size = (
+        background_item.size["width"], background_item.size["height"]
+    )
+    slider_size = slider_item.size["width"], slider_item.size["height"]
+    #slider_size = 61, 160
+    print(slider_size)
+    print(backgroud_size)
+    download_img(driver,background_item, background_path, backgroud_size)
+    download_img(driver,slider_item, 'imgs/'+rtime+'_'+slider_path, slider_size)
+  
 
-    def imageCompare(self,imagePath):
-        '''
-        :param imagePath: 图片存放的路径
-        :param threshold: 阈值,指定int类型
-        :return: 
-        '''
-        dataset_dir='imgs/'
-        # 使用imutils.paths模块中的list_images()方法可以获取当前目录下的图片路径
-        imagePaths=sorted(list(paths.list_images(dataset_dir)))
-        for image_filename in imagePaths:
+def is_image_file(self,filename):
+    return any(filename.endswith(extension) for extension in ['.png', '.jpg', '.jpeg', '.PNG', '.JPG', '.JPEG'])
+
+
+
+
+def imageCompare(imagePath):
+
+    dataset_dir='imgs/'
+    # 使用imutils.paths模块中的list_images()方法可以获取当前目录下的图片路径
+    imagePaths=sorted(list(paths.list_images(dataset_dir)))
+    with open(imagePath, 'rb') as fp1:
+            data1 = fp1.read()
+    file_md51= hashlib.md5(data1).hexdigest()
+    print(file_md51)
+    print('=====11111111111====')
+    for image_filename in imagePaths:
+
+        if imagePath not in  image_filename:
+
+            with open(image_filename, 'rb') as fp2:
+
+                data2 = fp2.read()
+            file_md52= hashlib.md5(data2).hexdigest()
       
-            if image_filename!=imagePath:
-                print(image_filename)
-                print(imagePath)
-                img1 = cv2.resize(cv2.imread(image_filename), (55, 140)) # 图片尺寸根据实际图片写入
-                img2 = cv2.resize(cv2.imread(imagePath), (55, 140))
-                confidence = cal_ccoeff_confidence(img1, img2)
-                if confidence > threshold:
-                    return image_filename
-                else:
-                    pass
-        return "F"
-
-
-    def get_distance_by_default(self,rtime):
-   
-        self._download_images(rtime)
-    
-        res=self.imageCompare('imgs/'+rtime+'_'+self.slider_path)
-        if res!='F':
-            print(res)
-        # load the picture
-        backgroud_img = Image.open(self.background_path).convert("L")
-        slider_img = Image.open(rtime+'_'+self.slider_path).convert("L")
-        backgroud_img = np.array(backgroud_img)
-        slider_img = np.array(slider_img)
-        # covld
-        top, bottom, left = get_boundary(slider_img)
-        scharr = np.array([[-6, 0, +6], [-6, 0, +6], [-6, 0, +6]])
-        grad = signal.correlate2d(
-            backgroud_img[top:bottom, :], scharr, boundary="symm", mode="same"
-        )
-        # 得到距离
-        cols_sum = np.sum(grad, axis=0)
-        # desc
-        sorted_sum = np.argsort(-np.abs(cols_sum))
-        distance = np.min(sorted_sum[:4])
-
-      
-        # show the distance on background
-        # valid_test_img_show(backgroud_img, distance)
-        return distance + self.offset - left+2
-
-
-
-
-
-    def run(self):
-    
-        for i in range(1000):
-            fore = random.randint(10, 18)
-            
-            print('fore:'+str(fore))
-            distance=0
-            try:
-                ts=calendar.timegm(time.gmtime())
-            
-                for i in range(5):
-                    distance = self.get_distance_by_default(str(ts))
-                print(distance)
-                if int(distance)<0:
-                    raise exception("errror distance")
-                print('offset:'+str(cfg.offset))
-                print(str(ts)+'_'+self.slider_path+'|'+str(distance))
-                with open("data.dic", "a") as f1:  # 保存图片到本地
-                    f1.write('\n'+str(ts)+'_'+self.slider_path+'|'+str(distance))
-                f1.close()
-            
-                tracks = get_tracks(distance + fore)
-
-                # 开始滑动
-                ActionChains(self.driver).click_and_hold(self.slider_btn).perform()
-                time.sleep(random.randint(0, 10) * 0.1)
-
-                for t in tracks:
-                
-                    ActionChains(self.driver, duration=5).move_by_offset(t, 0).perform()
-                time.sleep(random.randint(0, 10) * 0.1)
-                time.sleep(5)
-                
-                # 反拉
-                # tracks = get_tracks(fore)
-                # for t in tracks:
-                #     ActionChains(self.driver).move_by_offset(-t, 0).perform()
-
-                time.sleep(random.randint(0, 10) * 0.1)
-                ActionChains(self.driver).release().perform()
-                time.sleep(8)
-            except Exception as e:
-                print(e)
-                parser = argparse.ArgumentParser()
-                parser.add_argument(
-                    "--name",
-                    default="yidun",
-                    type=str,
-                    help="find useful name in setting.py"
-                )
-                args = parser.parse_args()
-
-                cfg = Config(args.name)
-                self._prepare(cfg)
-                pass
-        self.driver.close()
+            print(file_md52)
+            if file_md51==file_md52:
              
-                
+                print(image_filename)
+                return image_filename
+    return "F"
+
+
+def get_distance_by_default(rtime,offset,background_item,slider_item):
+
+    download_images(driver,background_item,slider_item,rtime)
+    res=imageCompare('imgs/'+rtime+'_'+slider_path)
     
+    if res!='F':
+ 
+        f = open("distince_data.dic")
+        lines = f.readlines()
+        for line in lines:
+            
+            dis=line.replace('\n','').split('|')
+            print(res)
+            print(dis[0])
+            if  dis[0] in (res):
+                print('distince:'+dis[1])
+                return int(dis[1])
+    # load the picture
+    backgroud_img = Image.open(background_path).convert("L")
+    slider_img = Image.open('imgs/'+rtime+'_'+slider_path).convert("L")
+    backgroud_img = np.array(backgroud_img)
+    slider_img = np.array(slider_img)
+    # covld
+    top, bottom, left = get_boundary(slider_img)
+    scharr = np.array([[-6, 0, +6], [-6, 0, +6], [-6, 0, +6]])
+    grad = signal.correlate2d(
+        backgroud_img[top:bottom, :], scharr, boundary="symm", mode="same"
+    )
+    # 得到距离
+    cols_sum = np.sum(grad, axis=0)
+    # desc
+    sorted_sum = np.argsort(-np.abs(cols_sum))
+    distance = np.min(sorted_sum[:4])
+
+    return distance + offset - left+3
+
+def runData(driver):
+    #rs=crack_slider(driver,account,passwd)
+    wait = WebDriverWait(driver, 5)
+    time.sleep(2)
+    driver.refresh()
+    # 点击对应标签
+    button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'body > main > div.g-bd > div > div.g-mn2 > div.m-tcapt > ul > li:nth-child(2)')))
+    button.click()
+    tc_item = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.yidun_intelli-tips')))
+    tc_item.click()
+
+
+
+# def runData(driver,account,passwd):
+#     #rs=crack_slider(driver,account,passwd)
+#     time.sleep(2)
+#     driver.delete_all_cookies()
+#     driver.refresh()
+#     time.sleep(3)
+#     submit = driver.find_element(By.XPATH,'//*[@id="root"]/div[1]/div/div[1]/img[3]')
+#     submit.click()
+#     time.sleep(2)
+#     username = driver.find_element(By.XPATH,'//*[@id="root"]/div[2]/div/div[2]/input[1]')  # 寻找账号输入框
+#     username.clear()
+#     username.send_keys(account)  # 填入用户名 
+#     password = driver.find_element(By.XPATH,'//*[@id="root"]/div[2]/div/div[2]/input[2]') # 寻找密码输入框
+#     password.clear()
+#     password.send_keys(passwd)  # 填入密码
+#     submit = driver.find_element(By.XPATH,'//*[@id="root"]/div[2]/div/div[2]/span')
+#     submit.click()
+#     time.sleep(3)
+    
+
+def run(driver):
+    time.sleep(3)
+   
+    wait = WebDriverWait(driver, 5)
+    background_item = wait.until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".yidun_bg-img"))
+    )
+    slider_item = wait.until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".yidun_jigsaw"))
+    )
+
+    resr=''
+    fore = random.randint(10, 18)
+
+    try:
+        distance=0
+        ts=calendar.timegm(time.gmtime())
+
+        distance = get_distance_by_default(str(ts),offset,background_item,slider_item)
+        resr=str(ts)+'_'+slider_path+'|'+str(distance)
+        mysetp=20
+        if distance>60 and  distance<=80:
+            mysetp=27
+        if distance>80 and distance<=120:
+            mysetp=37
+        elif distance>120 and distance<=160:
+            mysetp=57
+        elif distance>160:
+            mysetp=77
+   
+        distance=distance-2*mysetp
+       
+        try:
+            tracks = get_tracks(distance + fore)
+            print(resr)
+            print(str(mysetp))
+            # 开始滑动
+
+            slider_btn = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[class='yidun_slider__icon']")))
+
+            ActionChains(driver).click_and_hold(slider_btn).perform()
+            print('================================')
+            time.sleep(random.randint(1, 10) * 0.1)
+          
+            list=[0,1,1,mysetp,mysetp]
+            for ll in list:
+               ActionChains(driver,duration=5).move_by_offset(ll, 0).perform()
+            
+            print('||||')
+      
+            for t in tracks:
+                ActionChains(driver,duration=5).move_by_offset(t, 0).perform()
+   
+            time.sleep(random.randint(1, 5) * 0.1)
+            for t in range(1,3):
+              
+                ActionChains(driver,duration=5).move_by_offset(-3, 0).perform()
+                time.sleep(random.randint(1, 5) * 0.1)
+
+            slider_btn = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[class='yidun_slider__icon']")))
+            ActionChains(driver).click(slider_btn)
+            print('----------')
+            ActionChains(driver).release(on_element=None).perform()
+
+            time.sleep(2)
+        except Exception as e:
+        
+            print(e)
+            pass
+        try:
+            handlers = driver.window_handles
+            for winHandler in handlers:
+                driver.switch_to.default_content()            
+            time.sleep(3)
+            res=''
+            
+            if driver.find_element(By.XPATH,'//*[@id="body_container"]/div[*]/div/div/p[1]/img')!='':
+                
+                res=driver.find_element(By.CLASS_NAME,'msg__5sckD').get_attribute('textContent')
+                driver.find_element(By.XPATH,'//*[@id="body_container"]/div[*]/div/div/p[1]/img').click()
+                time.sleep(2)
+                print('存储拖动距离.....')
+                with open("distince_data.dic", "a") as f1:  # 保存图片到本地
+                    f1.write(resr+'\n')
+                f1.close()
+            print(res)
+   
+            if u'账号密码' in res :
+                return "F"
+        except Exception as e:
+            handlers = driver.window_handles
+            for winHandler in handlers:
+                driver.switch_to.default_content()            
+            time.sleep(3)
+            res=''
+            
+            if driver.find_element(By.XPATH,'//*[@id="root"]/div[1]/div/div[2]')!='':
+                print('存储拖动距离.....')
+                print(resr)
+                with open("distince_data.dic", "a") as f1:  # 保存图片到本地
+                    f1.write(resr+'\n')
+                f1.close()
+                
+                return "T"
+        return "F"
+    except Exception as e:
+     
+        res=driver.find_element(By.CSS_SELECTOR, "[class='yidun_tips__text yidun-fallback__tip']").get_attribute('textContent')
+        
+        print(res)
+        if res=='验证成功':
+            print('存储拖动距离.....')
+            with open("distince_data.dic", "a") as f1:  # 保存图片到本地
+                f1.write(resr+'\n')
+            f1.close()
+        runData(driver)
+        run(driver)
+        
+           
+
+   
+
+if __name__ == "__main__":
+      
+    #TEST_URL = "https://8083331.vip/"
+    TEST_URL = "http://dun.163.com/trial/sense"
+    options = webdriver.ChromeOptions()
+    #options.add_argument("start-maximized")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    options.add_experimental_option('useAutomationExtension', False)
+
+    driver = webdriver.Chrome(
+        options=options
+    )
+
+    driver.get(TEST_URL)
+    count=1
+    global countLines
+    readlines = len(open('data.dic','r').readlines())
+    accounts = open('data.dic')
+  
+    userCount=0
+
+    for account in accounts:
+  
+        passwds = open('dic.dic')
+        userCount=userCount+1
+        #count=1
+        for passwd in passwds:
+            account = account.strip()
+            passwd = passwd.strip()
+            print("当前第"+str(userCount)+"条数据,"+account+"|"+passwd)
+            #runData(driver,account,passwd)
+            runData(driver)
+            result=run(driver)
+            count=2
+            #icount=icount+1
+            print(result)
+            if result=='T':
+                with open("account.dic", "a") as f: 
+                    f.write(account+'|'+passwd+'\n')
+                f.close()
+    driver.close()
